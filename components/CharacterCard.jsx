21f080f08fd80eee489545fd2ecd7296e86dc7d1
serdardogandijital <serdardogan43@gmail.com>
2025-04-15 17:11:33 +0300

    Add files via upload

---
+++ components/CharacterCard.jsx
@@ -0,0 +1,1 @@
+// Placeholder, kullanıma hazır
---
+++ pages/index.jsx
@@ -0,0 +1,11 @@
+
+import Test from '../components/Test';
+
+export default function Home() {
+  return (
+    <div>
+      <h1 style={{ textAlign: 'center', marginTop: 20 }}>Sen Ne Çıktın?</h1>
+      <Test />
+    </div>
+  );
+}
